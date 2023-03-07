c0df1a7da33a080b9ad4054643c734e4d216f275
Elvin <elvinmonteiro77@gmail.com>
2023-03-06 19:08:12 -0500

    add logic for playing one round of the game

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+//generate random weapon selection for computer
 function getComputerChoice() {
     const weapons = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * 3);
-    return console.log(weapons[random])
+    return weapons[random];
 }
 
-getComputerChoice()
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(`Player chose ${playerSelection}`);
+console.log(`Computer chose ${computerSelection}`)
+
+//simulate one round of the game and return results of the battle based on which weapons went against each other
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock'
+        && computerSelection === 'paper') {
+        console.log('Paper beats rock. The computer wins!');
+    } else if (playerSelection === 'rock'
+        && computerSelection === 'scissors') {
+        console.log('Rock beats scissors. Player wins!');
+    } else if (playerSelection === 'paper'
+        && computerSelection === 'rock') {
+        console.log('Paper beats rock. Player wins!');
+    } else if (playerSelection === 'paper'
+        && computerSelection === 'scissors') {
+        console.log('Scissors beats paper. The computer wins!');
+    } else if (playerSelection === 'scissors'
+        && computerSelection === 'rock') {
+        console.log('Rock beats scissors. The computer wins!');
+    } else if (playerSelection === 'scissors'
+        && computerSelection === 'paper') {
+        console.log('Scissors beats paper.Player wins!');
+    } else if (playerSelection === computerSelection) {
+        console.log('it\'s a tie...battle again');
+    } else {
+        console.log('something went wrong :(')
+    }
+}
+
+playRound(playerSelection, computerSelection);
