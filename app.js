aa8e12c23386941fcc73d52dc833ac5227fad656
elvin <elvinmonteiro77@gmail.com>
2023-06-29 13:32:22 -0400

    render selections and result of the game

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const rockBtn = document.querySelector('#rock-btn');
 const paperBtn = document.querySelector('#paper-btn');
 const scissorsBtn = document.querySelector('#scissors-btn');
+const infoContainer = document.querySelector('.info-container');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -23,42 +24,66 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock'
         && computerSelection === 'paper') {
         computerScore++;
-        return 'Paper beats rock. The computer wins!';
+        return 'The computer wins!';
     } else if (playerSelection === 'rock'
         && computerSelection === 'scissors') {
         playerScore++;
-        return 'Rock beats scissors. Player wins!';
+        return 'Player wins!';
     } else if (playerSelection === 'paper'
         && computerSelection === 'rock') {
         playerScore++;
-        return 'Paper beats rock. Player wins!';
+        return 'Player wins!';
     } else if (playerSelection === 'paper'
         && computerSelection === 'scissors') {
         computerScore++;
-        return 'Scissors beats paper. The computer wins!';
+        return 'The computer wins!';
     } else if (playerSelection === 'scissors'
         && computerSelection === 'rock') {
         computerScore++;
-        return 'Rock beats scissors. The computer wins!';
+        return 'The computer wins!';
     } else if (playerSelection === 'scissors'
         && computerSelection === 'paper') {
         playerScore++;
-        return 'Scissors beats paper.Player wins!';
+        return 'Player wins!';
     } else if (playerSelection === computerSelection) {
         return 'it\'s a tie...battle again';
     }
 }
 
 
-//player makes selection and one round of the game is simulated;
+/*
+player makes selection and one round of the game is simulated;
+once pressed render choices and result
+update score.
+*/
+function renderGame() {
+    const player = document.createElement('p');
+    player.textContent = `You chose: ${playerSelection}`;
+
+    const computer = document.createElement('p');
+    computer.textContent = `The computer chose: ${computerSelection}`;
+
+    const result = document.createElement('p');
+    result.textContent = `${playRound(playerSelection, computerSelection)}`;
+
+    infoContainer.append(player, computer, result);
+   }
+
+
 rockBtn.addEventListener('click', () => {
-    console.log(playRound('rock', getComputerChoice()));
+    playerSelection = 'rock';
+    computerSelection = getComputerChoice();
+    renderGame();
 });
 paperBtn.addEventListener('click', () => {
-    console.log(playRound('paper', getComputerChoice()));
+    playerSelection = 'paper';
+    computerSelection = getComputerChoice();
+    renderGame();
 });
 scissorsBtn.addEventListener('click', () => {
-    console.log(playRound('scissors', getComputerChoice()));
+    playerSelection = 'scissors';
+    computerSelection = getComputerChoice();
+    renderGame();
 });
 
 
